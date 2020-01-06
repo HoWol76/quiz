2dd9c09bcd4f255d0fc0add181392508234950c2
Holger Wolff <holger.wolff@monash.edu>
2020-01-06 16:34:35 +1100

    improve UI

--- quiz.py
+++ quiz.py
@@ -13,6 +13,8 @@ class QuestionFrame(tk.Frame):
             self.selectedVar = selectedVar
         else:
             self.selectedVar = tk.IntVar()
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
 
     def setQuestion(self, question):
         self.question = question
@@ -23,7 +25,7 @@ class QuestionFrame(tk.Frame):
     def populateQuestion(self):
         tk.Label(self,
             text = self.question
-        ).grid()
+        ).grid(row=0, column=0, columnspan=2, sticky=[tk.W, tk.E])
         for i, ans in enumerate(self.answers):
             tk.Radiobutton(
                 self,
@@ -31,7 +33,7 @@ class QuestionFrame(tk.Frame):
                 variable=self.selectedVar,
                 value=i,
                 indicatoron=0
-            ).grid()
+            ).grid(column=1, row=i+1, sticky=[tk.W, tk.E])
 
     def getSelectedVar(self):
         return self.selectedVar
@@ -43,13 +45,33 @@ class Quiz(tk.Frame):
         self.master = master
         self.backend = backend
         self.selectedVar = tk.IntVar()
-        self.questionFrame = QuestionFrame(master=self, selectedVar=self.selectedVar)
         self.populateWindow()
         self.master.title("Quiz")
-        self.grid()
+        self.master.columnconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.grid(row=0, column=0, sticky=[tk.W, tk.E])
 
     def populateWindow(self):
 
+        # Submit Button
+        tk.Button(
+            self,
+            text="Submit", bg="green",
+            command=self.submitAnswer
+        ).grid(row=1, column=0, sticky=[tk.W, tk.E], padx=10, pady=5)
+        # Quit Button
+        tk.Button(
+            self,
+            text="Quit", bg="red",
+            command=self.master.destroy
+        ).grid(row=1, column=1, sticky=[tk.W, tk.E], padx=10, pady=5)
+
+        self.populateQuestionFrame()
+
+    def populateQuestionFrame(self):
+        self.selectedVar.set(-1)
+        self.questionFrame = QuestionFrame(master=self, selectedVar=self.selectedVar)
         self.questionFrame.setQuestion(
             self.backend.getQuestion()
         )
@@ -57,24 +79,10 @@ class Quiz(tk.Frame):
             self.backend.getAnswers()
         )
         self.questionFrame.populateQuestion()
-        self.questionFrame.grid()
-
-        # Submit Button
-        tk.Button(
-            self,
-            text="Submit", bg="green",
-            command=self.submitAnswer
-        ).grid()
-        # Quit Button
-        tk.Button(
-            self,
-            text="Quit", bg="red",
-            command=self.master.destroy
-        ).grid()
+        self.questionFrame.grid(row=0, columnspan=2, sticky=[tk.W, tk.E])
 
     def submitAnswer(self):
         answerIndex = self.selectedVar.get()
-        print(f"Selected answer index: {answerIndex}")
         correct = self.backend.checkAnswerByIndex(answerIndex)
         if correct:
             messagebox.showinfo(message="Correct!")
@@ -83,15 +91,7 @@ class Quiz(tk.Frame):
         try:
             self.backend.nextQuestion()
             self.questionFrame.destroy()
-            self.questionFrame = QuestionFrame(master=self, selectedVar = self.selectedVar)
-            self.questionFrame.setQuestion(
-                self.backend.getQuestion()
-            )
-            self.questionFrame.setAnswers(
-                self.backend.getAnswers()
-            )
-            self.questionFrame.populateQuestion()
-            self.questionFrame.grid()
+            self.populateQuestionFrame()
         except QuizOverException:
             messagebox.showinfo(message="The quiz is over. Thank you.")
             self.master.destroy()
@@ -99,6 +99,7 @@ class Quiz(tk.Frame):
 
 if __name__ == '__main__':
     root = tk.Tk()
+    root.title("Quiz")
     backend = QuizBackend()
     quiz = Quiz(master=root, backend=backend)
     quiz.mainloop()
